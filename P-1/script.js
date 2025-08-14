31598db77cf999142b622beebbff3d7e174bf3b7
kaifyyy <mkaifm728@gmail.com>
2025-08-14 23:16:01 +0530

    more Commit

--- P-1/script.js
+++ P-1/script.js
@@ -1,2 +1,17 @@
 let container = document.getElementById("container");
 
+let button = document.getElementbyId("change-btn");
+
+
+// HeX Code for Color - 
+
+
+
+button.onclick = () => {
+    console.log("Button Clicked");
+
+    // change the background Color
+
+
+
+}
