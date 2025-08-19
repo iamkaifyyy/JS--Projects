726bd194aa668b5ed5db426ee6a917f9ee7c9f32
kaifyyy <mkaifm728@gmail.com>
2025-08-20 00:01:11 +0530

    more changes

--- P3- RandomColorGenerator/script.js
+++ P3- RandomColorGenerator/script.js
@@ -1,30 +1,32 @@
 // generate random color
 
-const randomColor = function(){
-    const hex = "0123456789ABCDEF";
-    let color = '#';
-    for(let i = 0; i< 6; i++){
-        color += hex(Math.floor(Math.random() * 16));
-    }
-    return color;
+const randomColor = function () {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex(Math.floor(Math.random() * 16));
+  }
+  return color;
 };
 
+let intervalId;
+const startChangingColor = function () {
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 1000);
+  }
 
-const startChangingColor = function(){
+  function changeBgColor() {
     document.body.style.backgroundColor = randomColor();
+  }
 };
 
-
-
-
-const stopChangingColor = function(){
-    document.body.style.backgroundColor = randomColor();
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
 };
 
+document.querySelector("#start").addEventListener("click", startChangingColor);
 
-
-document.querySelector("#start").addEventListener('click', startChangingColor)
-
-document.querySelector("#stop").addEventListener('click',stopChangingColor);
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
 
 // console.log(Math.floor(Math.random() * 16));
